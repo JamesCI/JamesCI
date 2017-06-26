21549d84c68000dee8f961b82668af5c1b2f2c60
Alexander Haase <ahaase@alexhaase.de>
2017-06-26 05:33:09 +0200

    Implemented pipeline allocation in dispatcher.
    The dispatcher now tries to allocate a new pipeline ID, whenever it is called in
    a post-receive hook. The pipeline ID will be genereated per-project and
    concurrent tries handled in a limited fashion.

--- src/dispatcher/dispatcher.py
+++ src/dispatcher/dispatcher.py
@@ -21,8 +21,11 @@
 #
 
 import argparse
+import configparser
 import git
+import os
 import sys
+import time
 import yaml
 
 
@@ -30,13 +33,26 @@ import yaml
 # mandatory arguments are missing, an error message will be printed by the
 # argparse parser and this script exited immediately.
 argparser = argparse.ArgumentParser()
+argparser.add_argument('project', metavar='NAME',
+                       help='Project name (i.e. the repositorie\'s name).')
 argparser.add_argument('revision',
                        help='Git revision the jobs are launched for.')
+argparser.add_argument('--config', '-c', metavar='FILE',
+                       help='Configuration to use for the dispatcher.')
 argparser.add_argument('--type', '-t', choices=['branch', 'tag'],
                        metavar='TYPE', default='branch',
                        help='Type of the build. May be either branch or tag.')
 
 args = argparser.parse_args()
+
+
+# Read the configuration file, where additional information like the data
+# directory or a handler to use is stored. The system wide configuration will be
+# used before the file passed by command line. If no configuration file could be
+# loaded, an error message will be printed and the script exited immediately.
+config = configparser.ConfigParser()
+if not config.read(['/etc/james-ci.conf', args.config]):
+    sys.exit('Could not load any configuration file.')
 
 
 # Get the repository of the current working directory. As this command should be
@@ -53,8 +69,8 @@ try:
     # Get the contents of the James CI configuration file in the given revision.
     # Errors about invalid revisions and not available files will be handled
     # below.
-    config = yaml.load(repository.tree(args.revision)
-                       ['.james-ci.yml'].data_stream)
+    job_config = yaml.load(repository.tree(args.revision)
+                           ['.james-ci.yml'].data_stream)
 
 except git.exc.InvalidGitRepositoryError:
     # If the repository couldn't be opened, exit with an error message. This
@@ -76,3 +92,37 @@ except yaml.scanner.ScannerError:
     # Exit with an error message, if the James CI configuration file could not
     # be parsed.
     sys.exit('Could not parse the configuration file for James CI.')
+
+
+# Create a directory for the new pipeline. If this is the first job for a
+# project, the root directory for this project will be created and the initial
+# pipeline number be 1. There will be up to 3 retries, if a concurrent
+# post-receive creates the same pipeline number we'd like to use.
+project_dir = config['general']['data_dir'] + '/' + args.project
+pipeline = 1
+retries = 3
+for i in range(retries):
+    if os.path.exists(project_dir):
+        pipelines = os.listdir(project_dir)
+        if pipelines:
+            pipeline = max(map(int, pipelines)) + 1
+
+    try:
+        # Try tp create a new directory for this pipeline.
+        pipeline_dir = project_dir + '/' + str(pipeline)
+        os.makedirs(pipeline_dir)
+        break
+
+    except FileExistsError:
+        # Another process created this pieline concurrently. Retry to create a
+        # pipeline until the limit is reached.
+        if (i == retries - 1):
+            sys.exit('Failed to create a new pipeline.')
+        continue
+
+# Store the pipeline's configuration file in the pipeline's root directory. This
+# file will be used by the runner and UI. In addition the current timestamp will
+# be stored in this file, so the UI can display the submission time.
+job_config['created_at'] = int(time.time())
+yaml.dump(job_config, open(pipeline_dir + '/pipeline.yaml', 'w'),
+          default_flow_style=False)
